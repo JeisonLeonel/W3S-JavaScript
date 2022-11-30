0b09d6656c8f224487827f433c201eacfbf54d28
JeisonLeonel <jeisonleonel0814@gmail.com>
2022-11-29 19:26:47 -0500

    Se inicia curso de JavaScript

---
+++ js/bombilla.js
@@ -0,0 +1,7 @@
+function enceder(){
+    document.getElementById('myImg').src='../img/pic_bulbon.gif';
+}
+
+function apagar(){
+    document.getElementById('myImg').src='../img/pic_bulboff.gif';
+}
---
+++ js/estilos.js
@@ -0,0 +1,9 @@
+function masTamaño(){
+    document.getElementById('demo1').style.fontSize = '35px';
+}
+function ocultar(){
+    document.getElementById('demo2').style.display = 'none';
+}
+function mostar(){
+    document.getElementById('demo3').style.display = 'block';   
+}
